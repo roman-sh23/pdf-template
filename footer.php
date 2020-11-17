d1a4b63ae3acea40f391082cdad82f9b52c7ae45
roman-sh23 <73102395+roman-sh23@users.noreply.github.com>
2020-11-17 19:05:16 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,93 @@
-<?php
+<table width="100%" class="footer-table">
+	<tr>
+		<td valign="top" width="60%">
+			<table width="100%">
+				<tr>
+					<td>
+						<table width="100%">
+							<tr>
+								<td width="90%">
+									<p style="font-size: 24px; font-weight:bold;">Fastlo</p>
+								</td>
+
+								<td align="center" width="10%" style="border: 2px solid #000;">
+									<p style="font-size: 24px; font-weight: bold;">N</p>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="border-right: 2px solid #000;">
+						<table width="100%">
+							<tr>
+								<td valign="top" height="100px">
+									<p>...<br>7303 ...</p>
+									<p>...<br>0505707289</p>
+								</td>
+							</tr>
+
+							<tr>
+								<td>
+									<p>reference</p>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="border: 2px solid #000;">
+						<p style="font-size: 16px; font-weight: bold;">Ay7aja</p>
+					</td>
+				</tr>
+			</table>
+		</td>
+
+		<td valign="top" width="40%">
+			<table width="100%">
+				<tr>
+					<td align="center" style="color: #fff; background-color: #000;">
+						<p style="font-size: 30px; font-weight:bold;">SORTING</p>
+					</td>
+				</tr>
+
+				<tr>
+					<td align="center" style="color: #fff; background-color: #000;">
+						<p style="font-size: 18px; font-weight:bold;">262777374</p>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<table width="100%">
+							<tr>
+								<td>
+									<h4 style="font-size: 16px;">1/1</h4>
+									<p>25-07-2020<br>0.00 kg<br>211-1630</p>
+								</td>
+
+								<td>
+									<p>QR CODE</p>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<p style="font-size: 16px; font-weight: bold;">Cash: 344</p>
+					</td>
+				</tr>
+
+				<tr>
+					<td align="center" style="border: 2px solid #000;">
+						<p style="font-size: 16px; font-weight: bold;">Track 20837664</p>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
